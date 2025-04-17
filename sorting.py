ac8174e2867d511d9746ff0a6d0796348c51a438
Tereza Zendulkova <teri.zendulkova@gmail.com>
2025-04-17 11:56:52 +0200

    Sorting

--- sorting.py
+++ sorting.py
@@ -1,5 +1,5 @@
 import os
-
+import csv
 
 def read_data(file_name):
     """
@@ -10,7 +10,38 @@ def read_data(file_name):
     """
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path, "r") as f:
+        reader = csv.DictReader(f) ## jednotlive hodnoty z radku se priradi pod klic ktery je v hlavicce
+        data = {"series_1":[], "series_2":[], "series_3":[]}
+        for row in reader:
+            # print(row)
+            for key, value in row.items():
+                # print(key)
+                # print(value)
+                data[key].append(int(value))
+    return data
 
+
+print(read_data("V:/BPC - PRG2/lecture_sorting/numbers.csv"))
+
+def selection_sort(numbers, direction):
+    for i in range(len(numbers)):
+        min_index = i
+        if direction == "up":
+            for j in range(i + 1, len(numbers)):
+                if numbers[j] < numbers[min_index]:
+                    min_index = j
+            numbers[i], numbers[min_index] = numbers[min_index], numbers[i]
+        if direction == "down":
+            for j in range(i + 1, len(numbers)):
+                if numbers[j] > numbers[min_index]:
+                    min_index = j
+            numbers[i], numbers[min_index] = numbers[min_index], numbers[i]
+    return numbers
+
+
+numbers = [1,9,5,12,36,29,96,85,2,0,87]
+print(selection_sort(numbers, "down"))
 
 def main():
     pass
